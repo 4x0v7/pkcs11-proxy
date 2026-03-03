151d1d72e06e9937f562737e47517e01a8659d1d
407 <g4x0v7@gmail.com>
2026-03-03 18:47:35 +1300

    fix: increase max session count from 1 to 16
    Cosign's PKCS#11 library opens multiple concurrent sessions during
    key lookup and signing. The limit of 1 caused CKR_SESSION_COUNT (0xB1).

--- include/config.h
+++ include/config.h
@@ -4,7 +4,7 @@
 # define DEBUG_OUTPUT 0 // change to 1 to enable debugging
 
 # define PKCS11PROXY_LISTEN_BACKLOG 128
-# define PKCS11PROXY_MAX_SESSION_COUNT 1
+# define PKCS11PROXY_MAX_SESSION_COUNT 16
 
 // TLS 1.3 ciphersuites are configured in gck-rpc-tls.c
 
